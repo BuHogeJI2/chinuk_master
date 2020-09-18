7ec96cafd0aba759d63f063bf361493409c2ea49
BuHogeJI <dmitrydemidovich@yanex.ru>
2020-09-18 22:01:46 +0300

    create 2 new filters for positive and negative range

--- ChinukMaster/billing/templatetags/filters.py
+++ ChinukMaster/billing/templatetags/filters.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter(name='times')
 def times(value):
     return range(1, value+1)
+
+@register.filter(name='pos_3_times')
+def pos_3_times(value):
+    return range(1, 4)
+
+@register.filter(name='neg_3_times')
+def neg_3_times(value):
+    return range(-3, 0)
